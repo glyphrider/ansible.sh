76746a547bb435e7ce1ae58fa22d2ade64558612
Brian H. Ward <glyphrider@gmail.com>
2018-01-02 16:29:18 -0500

    initial commit

---
+++ README.md
@@ -0,0 +1,52 @@
+# ansible.sh
+
+A convenient shell system to utilize an ansible container for deployments.
+
+## How to use
+
+### Installation
+
+The script is designed to be consumed by `bash` each time the shell is invoked. Thus, it should be sourced into your `~/.bashrc` script. The following steps will achieve this result.
+
+```bash
+mkdir -p ~/work
+cd ~/work
+git clone https://github.com/glyphrider/ansible.sh.git
+echo 'source ~/work/ansible.sh/ansible.sh' >> ~/.bashrc
+```
+
+You will need to identify your ansible playbook location. If, for example, you have your playbook(s) in the directory `~/work/ansible_plays`, then you would configure this with the following:
+
+```bash
+echo 'ANSIBLE_PLAYBOOK=~/work/ansible_plays' > ~./ansible_playbook
+```
+
+If you don't configure your ansible playbook, then the script will attempt to do that the first time it runs. It will try to find a directory that is a git repository with a remote that matches `VHTx/ansible_deployment`. If you have such directory, then this value will be set for you automatically.
+
+### Pre-Requisites
+
+You must be running the `ssh-agent` and have the necessary keys loaded. Without this, ansible playbooks will not have reliable access the servers they create.
+
+### Environments
+
+You will need to configure some number of _environments_ for ansible. Create any number of files of the form ~/.ansible-key._environment-name_. These text files should contain a setting of the AWS_ACCESS_KEY_ID and AWS_SECRET_ACCESS_KEY variables, as well as an AWS_ACCOUNT_COLOR setting. An example file might be `~/.ansible-key.production` and contain the following:
+
+```bash
+AWS_ACCESS_KEY_ID="SOMELEGITIMATEACCESSID"
+AWS_SECRET_ACCESS_KEY="SOMESUPERSECRETACCEESSKEY"
+AWS_ACCOUNT_COLOR=red
+```
+
+## Get your shell on
+
+Once all this is done, you will be ready to use the script.
+
+Restart bash (`exec $SHELL`). If you are not using bash as your default shell, this won't work; however, you can use `SHELL=/bin/bash exec /bin/bash` to overcome your shell inadequacies.
+
+## What just happened
+
+A quick check of `alias` will show a new alias for each of your ~/.ansible-key._environment-name_ files. Each alias will match _environment-name_ and will setup ansible to use your new environment.
+
+The super obnoxious prompt is intensional.
+
+To stop using ansible, just `exit` the shell.
---
+++ ansible.sh
@@ -0,0 +1,90 @@
+#!/bin/bash
+
+ANSIBLE_PLAYBOOK_REPO_PATTERN="VHTx/ansible_deployment"
+ANSIBLE_DOCKER_IMAGE_TAG="virtualhold/ansible"
+
+function aws_common {
+  # export relevant environment variables
+  export AWS_ACCESS_KEY_ID
+  export AWS_SECRET_ACCESS_KEY
+  export AWS_REGION=${AWS_REGION:-us-east-1}
+
+  # setup a wicked-cool prompt
+  PS1="$(prompt_effect ${1}_bg)$(prompt_effect black)\t($(date +%Z)) | aws:$(prompt_effect bold)${AWS_ACCOUNT}$(prompt_effect unbold)@\${AWS_REGION} | \h:\w$(prompt_effect default_bg)$(prompt_effect default)\n[\\!]\\$ "
+
+  # add the ansible-playbook alias that uses all the environment variables
+  alias ansible-playbook='docker run -ti --rm -v /etc/passwd:/etc/passwd -v /etc/group:/etc/group --user $(id -u):$(id -g) -v ${SSH_AUTH_SOCK}:${SSH_AUTH_SOCK} -e SSH_AUTH_SOCK=${SSH_AUTH_SOCK} -v ${ANSIBLE_PLAYBOOK}:/playbook -w /playbook -e HOME=/tmp -e AWS_ACCESS_KEY_ID=${AWS_ACCESS_KEY_ID} -e AWS_SECRET_ACCESS_KEY=${AWS_SECRET_ACCESS_KEY} -e AWS_REGION=${AWS_REGION} ${ANSIBLE_DOCKER_IMAGE_TAG}'
+}
+
+function prompt_effect {
+  case $1 in
+    black)
+      echo -n "\e[30m"
+      ;;
+    red)
+      echo -n "\e[31m"
+      ;;
+    green)
+      echo -n "\e[32m"
+      ;;
+    blue)
+      echo -n "\e[34m"
+      ;;
+    red_bg)
+      echo -n "\e[41m"
+      ;;
+    green_bg)
+      echo -n "\e[42m"
+      ;;
+    blue_bg)
+      echo -n "\e[44m"
+      ;;
+    bold)
+      echo -n "\e[1m"
+      ;;
+    unbold)
+      echo -n "\e[21m"
+      ;;
+    default)
+      echo -n "\e[39m"
+      ;;
+    default_bg)
+      echo -n "\e[49m"
+      ;;
+    *)
+      ;;
+  esac
+}
+
+function create_alias {
+  alias ${1}="AWS_ACCOUNT=${1} \${SHELL}"
+}
+
+if [ "${AWS_ACCOUNT}" -a -f ~/.ansible-key.${AWS_ACCOUNT} ]; then
+  source ~/.ansible-key.${AWS_ACCOUNT}
+  aws_common ${AWS_ACCOUNT_COLOR}
+else
+  # create aliases
+  for keyfile in ~/.ansible-key.*; do
+    create_alias ${keyfile##*.ansible-key.}
+  done
+  # clear these guys
+  unset AWS_ACCESS_KEY_ID
+  unset AWS_SECRET_ACCESS_KEY
+fi
+
+if [ -z "${ANSIBLE_PLAYBOOK}" ]; then
+  if [ -f ~/.ansible_playbook ]; then
+    source ~/.ansible_playbook
+  else
+    # this is a one-time thing
+    # you can avoid this by creating your own ~/.ansible_playbook file with the appropriate shell command(s)
+    echo "Finding your ANSIBLE_PLAYBOOK...."
+    ANSIBLE_PLAYBOOK=$(find ~ -type d -exec grep -q ${ANSIBLE_PLAYBOOK_REPO_PATTERN} {}/.git/config 2> /dev/null \; -print)
+    export ANSIBLE_PLAYBOOK
+    echo -e "$(prompt_effect red)Using ANSIBLE_PLAYBOOK=${ANSIBLE_PLAYBOOK}$(prompt_effect default)"
+    echo "Persisting to ~/.ansible_playbook"
+    # this is what creates the file
+    echo "export ANSIBLE_PLAYBOOK=${ANSIBLE_PLAYBOOK}" > ~/.ansible_playbook
+  fi
+fi
